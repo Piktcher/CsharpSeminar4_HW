9fb72379971ad3ea00d73f4ad0807bc61b2a8cb5
Pavel Ordenov <pavelordenov@yandex.ru>
2023-06-01 19:55:15 +0200

    Complete HW tasks

---
+++ Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double NumPower(int a, int b)
+{
+    double result = Math.Pow(a, b);
+    return result;
+}
+
+Console.Write("Введите число А, ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B, ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double powered = NumPower(num1, num2);
+Console.WriteLine("Результат возведения числа А в степень В равен " + powered);
---
+++ Task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int DigitSum (int number)
+{
+    int result = 0;
+    while (number != 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+
+    return result;
+}
+
+Console.Write("Введите число для расчета суммы его цифр, ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = DigitSum(num);
+Console.WriteLine("Сумма цифр введенного числа равна " + sum);
